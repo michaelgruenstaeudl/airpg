e409605489ad24ea66c1550a6daf077526976ef3
TilMeh <github@tilman-mehl.de>
2020-11-17 14:08:38 +0100

    trying to fix script path

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="airpg",
-	version="0.1.3",
+	version="0.1.5",
 	author="Tilman Mehl, Michael Gruenstaeudl",
 	author_email="tilmanmehl@zedat.fu-berlin.de, m.gruenstaeudl@fu-berlin.de",
     description="A package to automatically access the inverted repeats of archived plastid genomes",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/michaelgruenstaeudl/airpg',
-    download_url='https://github.com/michaelgruenstaeudl/airpg/archive/v0.1.3.tar.gz',
+    download_url='https://github.com/michaelgruenstaeudl/airpg/archive/v0.1.5.tar.gz',
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: BSD License',
@@ -25,15 +25,10 @@ setuptools.setup(
     python_requires='>=3.6',
     keywords='plastid genomes, inverted repeats, NCBI Nucleotide',
     license='BSD',
-    entry_points='''
-    [console_scripts]
-    airpg_retrieve=airpg.scripts.airpg_retrieve:main
-    airpg_analyze=airpg.scripts.airpg_analyze:main
-    ''',
     packages=['airpg'], # So that the subfolder 'airpg' is read immediately.
     #packages = find_packages(),
     install_requires=['biopython', 'ete3', 'entrezpy', 'pandas'],
-    scripts=glob.glob('scripts/*'),
+    scripts=['airpg/scripts/airpg_retrieve.py', 'airpg/scripts/airpg_analyze.py', 'airpg/scripts/airpg_generate_blacklist.py'],
     test_suite='setup.my_test_suite',
     include_package_data=True,
     zip_safe=False
