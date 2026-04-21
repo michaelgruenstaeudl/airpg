44324a397e0e4358d4cc799d7c49129464d4db98
michaelgruenstaeudl <m_gruenstaeudl@fhsu.edu>
2026-04-21 18:48:56 -0500

    Bugfixes 2026 - part 2

--- setup.py
+++ setup.py
@@ -1,46 +1,53 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="airpg",
-    version="1.1.8",
+    version="1.1.9",
     author="Michael Gruenstaeudl",
     author_email="m_gruenstaeudl@fhsu.edu",
     description="A package to automatically access the inverted repeats of archived plastid genomes",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/michaelgruenstaeudl/airpg',
-    #download_url='https://github.com/michaelgruenstaeudl/airpg/archive/v1.1.8.tar.gz',
+    url="https://github.com/michaelgruenstaeudl/airpg",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
         "Operating System :: POSIX",
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Bio-Informatics'
-        ],
-    python_requires='>=3.6',
-    keywords='plastid genomes, inverted repeats, NCBI Nucleotide',
-    license='GPLv3',
-    #packages=['airpg'], # So that the subfolder 'airpg' is read immediately.
-    packages = setuptools.find_packages(),
-        entry_points={
-            "console_scripts": [
-                    "airpg_identify.py=airpg.scripts.airpg_identify:cli",
-                    "airpg_analyze.py=airpg.scripts.airpg_analyze:cli",
-                    "airpg_confirm.py=airpg.scripts.airpg_confirm:cli",
-                    "airpg_update_blocklist.py=airpg.scripts.airpg_update_blocklist:cli",
-                    "airpg-identify=airpg.scripts.airpg_identify:cli",
-                    "airpg-analyze=airpg.scripts.airpg_analyze:cli",
-                    "airpg-confirm=airpg.scripts.airpg_confirm:cli",
-                    "airpg-update-blocklist=airpg.scripts.airpg_update_blocklist:cli"
-            ]
-        },
-    install_requires=['biopython', 'ete3', 'entrezpy', 'pandas', 'fuzzywuzzy', 'coloredlogs', 'python-Levenshtein', 'legacy-cgi'],
-    test_suite='setup.my_test_suite',
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    python_requires=">=3.6",
+    keywords="plastid genomes, inverted repeats, NCBI Nucleotide",
+    license="GPLv3",
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": [
+            "airpg_identify.py=airpg.scripts.airpg_identify:cli",
+            "airpg_analyze.py=airpg.scripts.airpg_analyze:cli",
+            "airpg_confirm.py=airpg.scripts.airpg_confirm:cli",
+            "airpg_update_blocklist.py=airpg.scripts.airpg_update_blocklist:cli",
+            "airpg-identify=airpg.scripts.airpg_identify:cli",
+            "airpg-analyze=airpg.scripts.airpg_analyze:cli",
+            "airpg-confirm=airpg.scripts.airpg_confirm:cli",
+            "airpg-update-blocklist=airpg.scripts.airpg_update_blocklist:cli",
+        ]
+    },
+    install_requires=[
+        "biopython",
+        "ete3",
+        "entrezpy",
+        "pandas",
+        "fuzzywuzzy",
+        "coloredlogs",
+        "python-Levenshtein",
+        "legacy-cgi",
+    ],
+    test_suite="setup.my_test_suite",
     include_package_data=True,
-    package_data={'': ['airpg/tutorials/*.md']},
-    zip_safe=False
+    package_data={"airpg": ["tutorials/*.md"]},
+    zip_safe=False,
 )
